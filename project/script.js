d3c652ad0abda53b200985840cda9b1dc22cc287
Clemens <clemens.pueringer@students.htl-leonding.ac.at>
2025-04-06 14:16:57 +0200

    Man kann jetzt das Pokemon direkt selektieren, das einzige was noch fehlt ist der Attacken Selektor

--- project/script.js
+++ project/script.js
@@ -177,7 +177,7 @@ function selectPoke(pokemonTeamNum){
     if(player1.team[pokemonTeamNum].poke.type.length>1){
         tempstring = `
     <div id = "pokeDetailInfo">
-        <div id = "pokeDetailInfoImg" onclick="changePoke(${pokemonTeamNum})">
+        <div id = "pokeDetailInfoImg" onclick="changePoke1(${pokemonTeamNum})">
             <img src="./media/img/pokémon/${player1.team[pokemonTeamNum].poke.name.toLocaleLowerCase()}.png" alt="">
         </div>
         <div id = "pokeDetailInfoLower">
@@ -211,7 +211,7 @@ function selectPoke(pokemonTeamNum){
     }else{
         tempstring = `
     <div id = "pokeDetailInfo">
-        <div id = "pokeDetailInfoImg" onclick="changePoke(${pokemonTeamNum})">
+        <div id = "pokeDetailInfoImg" onclick="changePoke1(${pokemonTeamNum})">
             <img src="./media/img/pokémon/${player1.team[pokemonTeamNum].poke.name.toLocaleLowerCase()}.png" alt="">
         </div>
         <div id = "pokeDetailInfoLower">
@@ -259,6 +259,99 @@ function changePokePerk1(pokemonTeamNum, num) {
 }
 function savePoke1(pokemonTeamNum){
     document.getElementById("selectionDetail").style.display = "none"
+    document.getElementById("pokeSelectorMini").innerHTML = `
+    <div class="pokeBox" onclick="selectPoke(0)">
+        <img src="./media/img/pokémon/${player1.team[0].poke.name.toLocaleLowerCase()}.png" alt="">
+    </div>
+    <div class="pokeBox" onclick="selectPoke(1)">
+        <img src="./media/img/pokémon/${player1.team[1].poke.name.toLocaleLowerCase()}.png" alt="">
+    </div>
+    <div class="pokeBox" onclick="selectPoke(2)">
+        <img src="./media/img/pokémon/${player1.team[2].poke.name.toLocaleLowerCase()}.png" alt="">
+    </div>`
+}
+function changePoke1(pokemonTeamNum){
+    if(player1.team[pokemonTeamNum].poke.type.length>1){
+         tempstring = `
+<div id = "PokeOverallSelector">
+    <div id = "PokeOverArrowL" onclick="changePokeOver1(${pokemonTeamNum},-1)"> <img src="./media/img/pokearrow.png" alt=""></div>
+    <div id = "pokeDetailInfoSelector">
+        <div id = "pokeDetailInfoImgSelector">
+            <img src="./media/img/pokémon/${player1.team[pokemonTeamNum].poke.name.toLocaleLowerCase()}.png" alt="">
+        </div>
+        <div id = "pokeDetailInfoLower">
+            <div id = "pokeDetailInfoName">${player1.team[pokemonTeamNum].poke.name}</div>
+           <div id = "pokeDetailInfoType2">
+                <img src="./media/img/types/${player1.team[pokemonTeamNum].poke.type[0]}.png" alt="">
+                <img src="./media/img/types/${player1.team[pokemonTeamNum].poke.type[1]}.png" alt="">
+            </div>
+            <div id = "pokeDetailInfoStats">
+            <div id = "pokeDetailInfoHP">${player1.team[pokemonTeamNum].hp} HP</div>
+            <div id = "pokeDetailInfoStamina">${player1.team[pokemonTeamNum].poke.stamina} Stamina</div>
+            <div id = "pokeDetailInfoMight">${player1.team[pokemonTeamNum].poke.might} Might</div>
+            <div id = "pokeDetailInfoResistance">${player1.team[pokemonTeamNum].poke.resistance} Resistance</div>
+            <div id = "pokeDetailInfoSpeed">${player1.team[pokemonTeamNum].poke.speed} Speed</div>
+        </div>
+    </div>
+    </div>
+    <div id = "PokeOverArrowR" onclick="changePokeOver1(${pokemonTeamNum},1)"> <img src="./media/img/pokearrow.png" alt=""></div>
+</div>
+<div id = "savePoke" onclick= "savePokeOver1(${pokemonTeamNum})"> Save </div>
+    `
+    }else{
+        tempstring = `
+        <div id = "PokeOverallSelector">
+    <div id = "PokeOverArrowL" onclick="changePokeOver1(${pokemonTeamNum},-1)"> <img src="./media/img/pokearrow.png" alt=""></div>
+    <div id = "pokeDetailInfoSelector">
+        <div id = "pokeDetailInfoImgSelector">
+            <img src="./media/img/pokémon/${player1.team[pokemonTeamNum].poke.name.toLocaleLowerCase()}.png" alt="">
+        </div>
+        <div id = "pokeDetailInfoLower">
+            <div id = "pokeDetailInfoName">${player1.team[pokemonTeamNum].poke.name}</div>
+            <div id = "pokeDetailInfoType1">
+                <img src="./media/img/types/${player1.team[pokemonTeamNum].poke.type[0]}.png" alt="">
+            </div>
+            <div id = "pokeDetailInfoStats">
+            <div id = "pokeDetailInfoHP">${player1.team[pokemonTeamNum].hp} HP</div>
+            <div id = "pokeDetailInfoStamina">${player1.team[pokemonTeamNum].poke.stamina} Stamina</div>
+            <div id = "pokeDetailInfoMight">${player1.team[pokemonTeamNum].poke.might} Might</div>
+            <div id = "pokeDetailInfoResistance">${player1.team[pokemonTeamNum].poke.resistance} Resistance</div>
+            <div id = "pokeDetailInfoSpeed">${player1.team[pokemonTeamNum].poke.speed} Speed</div>
+        </div>
+    </div>
+    </div>
+    <div id = "PokeOverArrowR" onclick="changePokeOver1(${pokemonTeamNum},1)"> <img src="./media/img/pokearrow.png" alt=""></div>
+</div>
+<div id = "savePoke" onclick= "savePokeOver1(${pokemonTeamNum})"> Save </div>
+    `
+    }
+    document.getElementById("selectionDetail").innerHTML = tempstring
+}
+function changePokeOver1(pokemonTeamNum, num) {
+    const pokemonList = pokémon.list;
+    let currentIndex = pokemonList.indexOf(player1.team[pokemonTeamNum].poke);
+    if (currentIndex === -1) {
+        currentIndex = 0; 
+    }
+    currentIndex = (currentIndex + num + pokemonList.length) % pokemonList.length;
+    player1.team[pokemonTeamNum].poke = pokemonList[currentIndex];
+    player1.team[pokemonTeamNum].hp = player1.team[pokemonTeamNum].poke.hp;
+    for (let i = 0; i < perks.pokemon_perks.length; i++) {
+        if (perks.pokemon_perks[i].type == player1.team[pokemonTeamNum].poke.type[0]) {
+            player1.team[pokemonTeamNum].perk = perks.pokemon_perks[i];
+            break;
+        }
+    }
+    player1.team[pokemonTeamNum].moves = [];
+    for (let i = 0; i < attacks.moves.length; i++) {
+        if (attacks.moves[i].type == player1.team[pokemonTeamNum].poke.type[0] || attacks.moves[i].type == player1.team[pokemonTeamNum].poke.type[1]) {
+            player1.team[pokemonTeamNum].moves.push(attacks.moves[i]);
+        }
+    }
+    changePoke1(pokemonTeamNum);
+}
+function savePokeOver1(pokemonTeamNum){
+    selectPoke(pokemonTeamNum);
     document.getElementById("pokeSelectorMini").innerHTML = `
     <div class="pokeBox" onclick="selectPoke(0)">
         <img src="./media/img/pokémon/${player1.team[0].poke.name.toLocaleLowerCase()}.png" alt="">
